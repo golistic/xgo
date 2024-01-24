7d17a64e654481e5f830073acf0a2acbcb8a3afe
Geert Vanderkelen <geert@vanderkelen.org>
2024-01-24 10:10:56 +0100

    feat(xtime): add MiddayForDate
    We add the `MiddayForDate()` and `UTCMiddayForDate()` to get midday for a
    specific date. The date is passed using year, month, and day, just like with
    `time.Date()`.

--- xtime/clock.go
+++ xtime/clock.go
@@ -14,10 +14,23 @@ func Midday() time.Time {
 	return time.Date(n.Year(), n.Month(), n.Day(), 12, 0, 0, 0, time.Local)
 }
 
-// UTCMidday returns the current time adjusted to midday (12:00:00) in UTC.
+// MiddayForDate returns the time corresponding to 12:00:00 on the given year, month, and day.
+// The time is returned in the local time zone.
+func MiddayForDate(year int, month time.Month, day int) time.Time {
+
+	return time.Date(year, month, day, 12, 0, 0, 0, time.Local)
+}
+
+// UTCMidday returns the time corresponding to 12:00:00 in UTC on the current UTC date.
 func UTCMidday() time.Time {
 
 	now := time.Now().UTC()
 
 	return time.Date(now.Year(), now.Month(), now.Day(), 12, 0, 0, 0, time.UTC)
+}
+
+// UTCMiddayForDate returns the time corresponding to 12:00:00 in UTC for the given year, month, and day.
+func UTCMiddayForDate(year int, month time.Month, day int) time.Time {
+
+	return time.Date(year, month, day, 12, 0, 0, 0, time.UTC)
 }
--- xtime/clock_test.go
+++ xtime/clock_test.go
@@ -13,43 +13,114 @@ import (
 
 func TestUTCMidday(t *testing.T) {
 
+	now := time.Now().UTC()
+	want := time.Date(now.Year(), now.Month(), now.Day(), 12, 0, 0, 0, now.Location())
+	xt.Eq(t, want, UTCMidday())
+}
+
+func TestUTCMiddayForDate(t *testing.T) {
+
+	now := time.Now().UTC()
+
 	tests := []struct {
-		name string
-		want time.Time
+		name      string
+		wantYear  int
+		wantMonth time.Month
+		wantDay   int
 	}{
 		{
-			name: "UTC_Midday_Today",
-			want: time.Date(time.Now().UTC().Year(), time.Now().UTC().Month(), time.Now().UTC().Day(), 12, 0, 0, 0, time.UTC),
+			name:      "past date",
+			wantYear:  now.Year() - 1,
+			wantMonth: now.Month(),
+			wantDay:   now.Day(),
+		},
+		{
+			name:      "future date",
+			wantYear:  now.Year() - 1,
+			wantMonth: now.Month(),
+			wantDay:   now.Day(),
+		},
+		{
+			name:      "now",
+			wantYear:  now.Year(),
+			wantMonth: now.Month(),
+			wantDay:   now.Day(),
+		},
+		{
+			name:      "month out of range",
+			wantYear:  now.Year(),
+			wantMonth: time.Month(13),
+			wantDay:   now.Day(),
+		},
+		{
+			name:      "day out of range",
+			wantYear:  now.Year(),
+			wantMonth: now.Month(),
+			wantDay:   32,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			xt.Eq(t, tt.want, UTCMidday())
+			want := time.Date(tt.wantYear, tt.wantMonth, tt.wantDay, 12, 0, 0, 0, time.UTC)
+			xt.Eq(t, want, UTCMiddayForDate(tt.wantYear, tt.wantMonth, tt.wantDay))
 		})
 	}
 }
 
 func TestMidday(t *testing.T) {
 
-	t.Parallel()
+	now := time.Now()
+	want := time.Date(now.Year(), now.Month(), now.Day(), 12, 0, 0, 0, now.Location())
+	xt.Eq(t, want, Midday())
+}
+
+func TestMiddayForDate(t *testing.T) {
+
+	now := time.Now()
 
 	tests := []struct {
-		name string
-		want func() time.Time
+		name      string
+		wantYear  int
+		wantMonth time.Month
+		wantDay   int
 	}{
 		{
-			name: "local midday",
-			want: func() time.Time {
-				n := time.Now()
-				return time.Date(n.Year(), n.Month(), n.Day(), 12, 0, 0, 0, n.Location())
-			},
+			name:      "past date",
+			wantYear:  now.Year() - 1,
+			wantMonth: now.Month(),
+			wantDay:   now.Day(),
+		},
+		{
+			name:      "future date",
+			wantYear:  now.Year() - 1,
+			wantMonth: now.Month(),
+			wantDay:   now.Day(),
+		},
+		{
+			name:      "now",
+			wantYear:  now.Year(),
+			wantMonth: now.Month(),
+			wantDay:   now.Day(),
+		},
+		{
+			name:      "month out of range",
+			wantYear:  now.Year(),
+			wantMonth: time.Month(13),
+			wantDay:   now.Day(),
+		},
+		{
+			name:      "day out of range",
+			wantYear:  now.Year(),
+			wantMonth: now.Month(),
+			wantDay:   32,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			xt.Eq(t, tt.want(), Midday())
+			want := time.Date(tt.wantYear, tt.wantMonth, tt.wantDay, 12, 0, 0, 0, now.Location())
+			xt.Eq(t, want, MiddayForDate(tt.wantYear, tt.wantMonth, tt.wantDay))
 		})
 	}
 }
